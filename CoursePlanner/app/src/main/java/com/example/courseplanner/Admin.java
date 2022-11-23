5f1b504ad98565e4bcef4a887894140e5a80d389
chenyo34 <2277208307@qq.com>
2022-11-23 18:56:59 -0500

    Delete the redundant codes

--- CoursePlanner/app/src/main/java/com/example/courseplanner/Admin.java
+++
@@ -1,9 +0,0 @@
-package com.example.courseplanner;
-
-public class Admin extends User{
-
-    public Admin(String name, String email, String UTORid) {
-        super(name, email, UTORid);
-    }
-
-}
