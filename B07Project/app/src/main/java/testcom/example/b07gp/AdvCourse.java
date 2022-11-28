466deab1550177bdda7aff9f5f82cc3a00a99f5b
chenyo34 <2277208307@qq.com>
2022-11-27 23:30:11 -0500

    Comment this redundant class

--- B07Project/app/src/main/java/testcom/example/b07gp/AdvCourse.java
+++ B07Project/app/src/main/java/testcom/example/b07gp/AdvCourse.java
@@ -2,25 +2,25 @@ package testcom.example.b07gp;
 
 import java.util.HashSet;
 
-public class AdvCourse extends Course{
+public class AdvCourse {
 
-    HashSet<String> Precourses;
-
-    public AdvCourse(String courseCode, String courseName, HashSet<String> offeringSessions) {
-        super(courseCode, courseName, offeringSessions);
-    }
-
-    public AdvCourse(String courseCode, String courseName, HashSet<String> offeringSessions, HashSet<String> precourses) {
-        super(courseCode, courseName, offeringSessions);
-        Precourses = precourses;
-    }
-
-    public HashSet<String> getPrecourses() {
-        return Precourses;
-    }
-
-    public void setPrecourses(HashSet<String> precourses) {
-        Precourses = precourses;
-    }
+//    HashSet<String> Precourses;
+//
+//    public AdvCourse(String courseCode, String courseName, HashSet<String> offeringSessions) {
+//        super(courseCode, courseName, offeringSessions);
+//    }
+//
+//    public AdvCourse(String courseCode, String courseName, HashSet<String> offeringSessions, HashSet<String> precourses) {
+//        super(courseCode, courseName, offeringSessions);
+//        Precourses = precourses;
+//    }
+//
+//    public ArrayList<String> getPrecourses() {
+//        return Precourses;
+//    }
+//
+//    public void setPrecourses(ArrayList<String> precourses) {
+//        Precourses = precourses;
+//    }
 
 }
