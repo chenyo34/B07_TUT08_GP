7f02c4083e40f2565b75b9a9291b5c160e71193e
chenyo34 <2277208307@qq.com>
2022-11-29 01:40:06 -0500

    timeline UI modify

--- B07Project/app/src/main/java/testcom/example/b07gp/StudentTimelineDisplay.java
+++ B07Project/app/src/main/java/testcom/example/b07gp/StudentTimelineDisplay.java
@@ -6,19 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class StudentTimelineDisplay extends AppCompatActivity implements View.OnClickListener {
 
-    private Button stuAddReturn;
+    private Button stuAddReturn, stuAddPlanCourse;
+    private EditText plannCourse;
+    private Model model;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_timeline_display);
 
-
         stuAddReturn = (Button) findViewById(R.id.Return_button);
         stuAddReturn.setOnClickListener(this);
+
+
+        stuAddPlanCourse = (Button) findViewById(R.id.timeline_add_button);
+        stuAddPlanCourse.setOnClickListener(this);
+
+        plannCourse = (EditText) findViewById(R.id.plannCourse);
+
+        mAuth = FirebaseAuth.getInstance();
+        model = new Model();
     }
 
 
@@ -28,7 +43,9 @@ public class StudentTimelineDisplay extends AppCompatActivity implements View.On
             case R.id.Return_button:
                 startActivity(new Intent(this, StudentActivity.class));
                 break;
-
+            case R.id.timeline_add_button:
+                System.out.println(mAuth.signInWithEmailAndPassword(""));
+                model.getCourse(plannCourse, );
         }
     }
 }
