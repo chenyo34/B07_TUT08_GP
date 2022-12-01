a04af412306ed76402548490bc07354bb1a71eaf
wxhwxh2003 <wxhwxhwxh2003@gmail.com>
2022-12-01 16:19:19 -0500

    timeline update

--- B07Project/app/src/main/java/testcom/example/b07gp/StudentTimelineTable.java
+++ B07Project/app/src/main/java/testcom/example/b07gp/StudentTimelineTable.java
@@ -9,42 +9,42 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class StudentTimelineTable extends AppCompatActivity implements View.OnClickListener{
+public class StudentTimelineTable extends AppCompatActivity {
 
     private Button stuTableReturn;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                finish();
+//                return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        return true;
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_timeline_table);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        stuTableReturn = (Button) findViewById(R.id.Timeline_table_return);
-        stuTableReturn.setOnClickListener(this);
+//        stuTableReturn = (Button) findViewById(R.id.Timeline_table_return);
+//        stuTableReturn.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.Timeline_table_return:
-                startActivity(new Intent(this, StudentTimelineDisplay.class));
-                break;
-        }
-    }
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()){
+//            case R.id.Timeline_table_return:
+//                startActivity(new Intent(this, StudentTimelineDisplay.class));
+//                break;
+//        }
+//    }
 }
