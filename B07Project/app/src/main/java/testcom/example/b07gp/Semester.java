146bf76b33eae29896d502a51da5e692045d3f69
chenyo34 <2277208307@qq.com>
2022-12-02 18:11:48 -0500

    Add Semester Class
    Co-Authored-By: EmmaGZH <114100413+EmmaGZH@users.noreply.github.com>
    Co-Authored-By: RebeccaH2003 <114100529+RebeccaH2003@users.noreply.github.com>
    Co-Authored-By: Frank Zhang <114182570+AoLiGei1221@users.noreply.github.com>
    Co-Authored-By: wxhwxh2003 <113959447+wxhwxh2003@users.noreply.github.com>

---
+++ B07Project/app/src/main/java/testcom/example/b07gp/Semester.java
@@ -0,0 +1,35 @@
+package testcom.example.b07gp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Semester {
+    int year;
+    String session;
+
+    public Semester() {
+        this.year = 2022;
+        this.session = "Fall";
+    }
+
+    public Semester(int year, String session) {
+        this.year = year;
+        this.session = session;
+    }
+
+    public Semester next() {
+
+        List<String> sessions = new ArrayList<>();
+        sessions.add("Fall"); sessions.add("Winter"); sessions.add("Summer");
+        int next = (sessions.indexOf(this.session)+1) % 3;
+
+        //Return the next semester by checking the current session
+        if (this.session.equals("Summer")) {
+            return new Semester(this.year+1,sessions.get(next));
+        }
+        return new Semester(this.year,sessions.get(next));
+
+    }
+
+
+}
