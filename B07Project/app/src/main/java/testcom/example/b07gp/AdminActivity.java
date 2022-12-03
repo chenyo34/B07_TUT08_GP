d2c31e46dd845f36fb38d09306cae4d1d0e2969b
RebeccaH2003 <114100529+RebeccaH2003@users.noreply.github.com>
2022-12-03 12:41:26 -0500

    Admin Activity UI Update

--- B07Project/app/src/main/java/testcom/example/b07gp/AdminActivity.java
+++ B07Project/app/src/main/java/testcom/example/b07gp/AdminActivity.java
@@ -1,16 +1,39 @@
 package testcom.example.b07gp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AdminActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button adminActLogOutButton, adminActCourseListDisplayButton,
             adminActEditDisplayButton, adminActAddCourseButton;
+    private TextView mailText, nameText;
+    private FirebaseAuth mAuth;
+    private String adminMail, adminName;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+
+    private FirebaseUser user;
+    private String Uid;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +46,41 @@ public class AdminActivity extends AppCompatActivity implements View.OnClickList
         adminActCourseListDisplayButton = (Button) findViewById(R.id.adminActCourseListDisplayButton);
         adminActCourseListDisplayButton.setOnClickListener(this);
 
-        adminActEditDisplayButton = (Button) findViewById(R.id.adminActEditDisplayButton);
-        adminActEditDisplayButton.setOnClickListener(this);
+//        adminActEditDisplayButton = (Button) findViewById(R.id.adminActEditDisplayButton);
+//        adminActEditDisplayButton.setOnClickListener(this);
+//
+//        adminActAddCourseButton = (Button) findViewById(R.id.adminActAddCourseButton);
+//        adminActAddCourseButton.setOnClickListener(this);
 
-        adminActAddCourseButton = (Button) findViewById(R.id.adminActAddCourseButton);
-        adminActAddCourseButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        mailText = (TextView) findViewById(R.id.adminMail);
+        nameText = (TextView) findViewById(R.id.adminName);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        Uid = user.getUid();
+       // Uid = "7B4qZVPdbuP0kbEisTgfXcaMdM72";
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Users");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                adminMail = snapshot.child(Uid).child("email").getValue(String.class);
+                adminName = snapshot.child(Uid).child("name").getValue(String.class);
+
+                mailText.setText(adminMail);
+                nameText.setText(adminName+"   \uD83D\uDC4B");
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
 
     }
 
@@ -42,14 +95,14 @@ public class AdminActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(new Intent(this,AdminListDisplay.class));
                 break;
             }
-            case R.id.adminActEditDisplayButton:{
-                startActivity(new Intent(this,AdminEditDisplay.class));
-                break;
-            }
-            case R.id.adminActAddCourseButton:{
-                startActivity(new Intent(this,AdminAddCourses.class));
-                break;
-            }
+//            case R.id.adminActEditDisplayButton:{
+//                startActivity(new Intent(this,test1.class));
+//                break;
+//            }
+//            case R.id.adminActAddCourseButton:{
+//                startActivity(new Intent(this,AdminAddCourses.class));
+//                break;
+//            }
         }
     }
 }
